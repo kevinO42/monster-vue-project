5b582161f021cb8d6f8d5fd417e23c608134cd3d
Kevin Kristoffer Oporto <kevin042@Kevins-MacBook-Pro.local>
2022-02-16 23:38:19 +0800

    BaseTable component

---
+++ src/components/BaseTable.vue
@@ -0,0 +1,37 @@
+<template>
+  <table class="table">
+    <thead>
+    <tr v-for="th in tableHeaders" :key="th">
+        <th scope="col">{{ th.name }}</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="td in tableEntries" :key="td">
+        <td>{{ td.avatar }}</td>
+        <td>{{ td.first_name + ' ' + td.last_name }}</td>
+        <td>{{ td.email }}</td>
+        <td>{{ td.date_of_birth }}</td>
+        <td>{{ td.title }}</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+    name: 'BaseTable',
+    props: {
+        headers: Array,
+        entries: Array
+    },
+    computed: {
+        tableHeaders() {
+            return this.headers || []
+        },
+
+        tableEntries() {
+            return this.entries || []
+        }
+    }
+}
+</script>
