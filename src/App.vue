eabf233f1df6b3ce894066ef4f4d30bcae5b76a0
Kevin Kristoffer Oporto <kevin042@Kevins-MacBook-Pro.local>
2022-02-16 21:32:39 +0800

    Header & Sidenav components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container">
+    <!-- header template -->
+    <Header />
+
+    <!-- sidebar navigation template -->
+    <Sidenav />
   </div>
+
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from './components/Header.vue'
+import Sidenav from './components/Sidenav.vue'
 
-#nav {
-  padding: 30px;
+export default {
+  components: {
+      Header,
+      Sidenav
+  }
 }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style src="./assets/css/style.css" lang="css" />
