37b40617cc5efb36e5f5f62e05d23b2941859f0d
Kevin Kristoffer Oporto <kevin042@Kevins-MacBook-Pro.local>
2022-02-17 02:30:57 +0800

    Apply template to Error Page

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -1,4 +1,10 @@
 <template>
-  <h1>Error 404</h1>
-  <h2>Page Not Found.</h2>
+  <div class="main">
+    <div class="card">
+      <div class="card-header"><h5>Error 404</h5></div>
+      <div class="card-body">
+        <b>Whoops! Page Not Found.</b>
+      </div>
+    </div>
+  </div>
 </template>
