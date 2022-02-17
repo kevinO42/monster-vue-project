73dd86c335ae45c0b2a6c7d3570e74a14c315907
Kevin Kristoffer Oporto <kevin042@Kevins-MacBook-Pro.local>
2022-02-17 23:34:01 +0800

    Create user component

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -3,7 +3,58 @@
       <div class="card">
           <div class="card-header"><h5>Create User</h5></div>
           <div class="card-body">
-              
+              <form @submit.prevent="handleSubmit" :class="!validated ? 'row g-3 needs-validation' : 'row g-3 was-validated'" novalidate>
+                <div class="col-md-4 has-validation">
+                    <label for="firstname" class="form-label">First name</label>
+                    <input type="text" v-model="form.firstname" class="form-control" id="firstname" placeholder="Enter firstname" required />
+                    <div v-if="errors.err_firstname" class="invalid-feedback">
+                        {{ errors.err_firstname }}
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <label for="lastname" class="form-label">Last name</label>
+                    <input type="text" v-model="form.lastname" class="form-control " id="lastname" placeholder="Enter lastname" required />
+                    <div v-if="errors.err_lastname" class="invalid-feedback">
+                        {{ errors.err_lastname }}
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" v-model="form.email" class="form-control" id="email" placeholder="Enter email" required />
+                    <div v-if="errors.err_email" class="invalid-feedback">
+                        {{ errors.err_email }}
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <label for="birthdate" class="form-label">Date of Birth</label>
+                    <input type="date" v-model="form.birthdate" class="form-control" id="birthdate" required />
+                    <div v-if="errors.err_birthdate" class="invalid-feedback">
+                        {{ errors.err_birthdate }}
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <label for="title" class="form-label">Employment Title</label>
+                    <input type="text" v-model="form.title" class="form-control" id="title" placeholder="Enter title" required />
+                    <div v-if="errors.err_lastname" class="invalid-feedback">
+                        {{ errors.err_title }}
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <label for="status" class="form-label">Subscription</label>
+                    <select v-model="form.status" class="form-select" id="status" required>
+                        <option value="Active">Active</option>
+                        <option value="Idle">Idle</option>
+                        <option value="Pending">Pending</option>
+                        <option value="Blocked">Blocked</option>
+                    </select>
+                    <div v-if="errors.err_lastname" class="invalid-feedback">
+                        {{ errors.err_status }}
+                    </div>
+                </div>
+                <div class="col-12">
+                    <button class="btn btn-primary" type="submit">Register User</button>
+                </div>
+              </form>
           </div>
       </div>
   </div>
@@ -11,6 +62,49 @@
 
 <script>
 export default {
+    data() {
+        return {
+            form: {
+                firstname: '',
+                lastname: '',
+                email: '',
+                birthdate: '',
+                title: '',
+                status: ''
+            },
+            errors: {
+                err_firstname: '',
+                err_lastname: '',
+                err_email: '',
+                err_birthdate: '',
+                err_title: '',
+                err_status: ''
+            },
+            validated: false
+        }
+    },
 
+    methods: {
+        handleValidation(error) {
+            // simple validation for demo purposes only
+            this.errors.err_firstname = error.err_firstname.length ? '' : 'This is a required field.'
+            this.errors.err_lastname = error.err_lastname.length ? '' : 'This is a required field.'
+            this.errors.err_email = error.err_email.length ? '' : 'This is a required field.'
+            this.errors.err_birthdate = error.err_birthdate.length ? '' : 'This is a required field.'
+            this.errors.err_title = error.err_title.length ? '' : 'This is a required field.'
+            this.errors.err_status = error.err_status.length ? '' : 'This is a required field.'
+        },
+
+        handleSubmit() {
+            // check validation process
+            this.validated = true
+
+            // execute validation
+            this.handleValidation(this.errors)
+
+            // display to console form
+            console.log(this.form)
+        }
+    }
 }
 </script>
